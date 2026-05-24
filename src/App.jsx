61e90edcd7b826b976c03a1ac04c8e461292fd1b
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-05-24 19:11:46 +0900

    📝 comment: add comments about routing and SPA
    - add comments about routing (client-side routing)
    - add comments about single-page application(SPA)

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,20 @@ function App() {
 }
 
 export default App;
+
+////////////////////////////////////////
+
+// ※ Routing (Client-Side Routing)?
+// - With routing, we matvh different URLs to different UI views(REact components): routes
+// - This enables users to navigate betwwen different applications screens, using the browser URL
+// - Keeps the UI sync with the current browser URL
+// - Allows us to build Single-Page-Applications
+// (in case React → using React Router)
+
+// ※ Single-Page Application (SPA)
+// - Application that is executed entirely on the client (browsers)
+// - Routes: different URLs correspond to different views (components)
+// - JavaScript (React) is used to update the page (DOM)
+// - This page is never reloaded
+// - Feels like a native app
+// - Additional data might be loaded from a web API
