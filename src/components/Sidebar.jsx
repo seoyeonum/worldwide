fe7c3d6368dc2202951388d78a67f1967cd8a686
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-05-29 23:58:11 +0900

    ✨ feat: implement nested and index routes
    - add nested routes and index route
    - connect AppNav using NavLink
    - render child routes with Outlet component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import AppNav from './AppNav';
 import Footer from './Footer';
 import Logo from './Logo';
@@ -8,8 +9,8 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-
-      <p>List of cities</p>
+      {/* Outlet 컴포넌트는 children props와 유사 */}
+      <Outlet />
       <Footer />
     </div>
   );
