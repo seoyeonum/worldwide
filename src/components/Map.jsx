e4f772903c2248a03ddf881d8e2c5e67b12e9893
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-06-04 23:00:08 +0900

    ✨ feat: implement programmatic navigation
    - add programmatic navigation with useNavigate
    - add programmatic navigation with Navigate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,20 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParamas] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate('form'); // 링크 클릭 없이 이동하는 프로그래밍 방식의 내비게이션
+      }}
+    >
       <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
