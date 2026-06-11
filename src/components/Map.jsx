94b645d232d719b0c96226650d9d1d6650546f49
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-06-11 23:07:32 +0900

    ✨ feat: add map integration using leaflet
    - install leaflet and react-leaflet packages
    - add example code to display the map
    - import Leaflet components for map rendering

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,11 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import styles from './Map.module.css';
+import { useState } from 'react';
 
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParamas] = useSearchParams();
   const lat = searchParams.get('lat');
@@ -15,13 +18,22 @@ function Map() {
         navigate('form'); // 링크 클릭 없이 이동하는 프로그래밍 방식의 내비게이션
       }}
     >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParamas({ lat: 23, lng: 50 })}>
-        Change Position
-      </button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
