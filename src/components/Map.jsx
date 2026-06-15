062ddacc8eabcd2a572988836c1f4bdb26f8b6aa
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-06-15 22:58:46 +0900

    ✨ feat: autofill city details from map position
    - add useUrlPosition hook to get position data from url
    - fetch city name and emoji from reverse geocoding API
    - add error message when the country code doesn't exist

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,20 +11,18 @@ import styles from './Map.module.css';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 import { useGeolocation } from '../hooks/useGeoloaction';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 import Button from './Button';
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geoloactionPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
